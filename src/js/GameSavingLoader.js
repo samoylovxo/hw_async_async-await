64f14be043587657dca35b591a7deaf82946d55b
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-07-11 22:42:37 +0300

    files added

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-empty-function */
+/* eslint-disable no-useless-constructor */
+import json from './parser';
+import read from './reader';
+
+export default class GameSavingLoader {
+  constructor() {}
+
+  async load() {
+    const resolveBuffer = await read();
+    const saveObj = await json(resolveBuffer);
+    return saveObj;
+  }
+}
